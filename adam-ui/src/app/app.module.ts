5f70154da41a26c068e41f603ce689aefa4b22e3
marinbalabanov <marincomics@gmail.com>
2020-08-05 15:05:16 +0300

    Added carousel, removed redundant components

--- adam-ui/src/app/app.module.ts
+++ adam-ui/src/app/app.module.ts
@@ -4,9 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
 import { IntroComponent } from './intro/intro.component';
-import { SearchComponent } from './search/search.component';
 import { ArchivelistComponent } from './archivelist/archivelist.component';
 import { FeaturesComponent } from './features/features.component';
 import { NewsComponent } from './news/news.component';
@@ -14,7 +12,6 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NewslistComponent } from './newslist/newslist.component';
 import { ArchiveComponent } from './archive/archive.component';
-import { StandardheaderComponent } from './standardheader/standardheader.component';
 import { NewsarticleComponent } from './newsarticle/newsarticle.component';
 import { ArchivedetailComponent } from './archivedetail/archivedetail.component';
 import { AboutComponent } from './about/about.component';
@@ -22,14 +19,13 @@ import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent,
     IntroComponent,
-    SearchComponent,
     ArchivelistComponent,
     FeaturesComponent,
     NewsComponent,
@@ -37,12 +33,12 @@ import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
     HomeComponent,
     NewslistComponent,
     ArchiveComponent,
-    StandardheaderComponent,
     NewsarticleComponent,
     ArchivedetailComponent,
     AboutComponent,
     ContactComponent,
     ItemdetailsComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
--- adam-ui/src/app/carousel/carousel.component.spec.ts
+++ adam-ui/src/app/carousel/carousel.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { CarouselComponent } from './carousel.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('CarouselComponent', () => {
+  let component: CarouselComponent;
+  let fixture: ComponentFixture<CarouselComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ CarouselComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
