57fff9f3c2f4da6d6a2637d54c234220cb8fbebc
marinbalabanov <marincomics@gmail.com>
2020-08-14 16:05:23 +0300

    Added static versions of edit and create forms, ammended data

--- adam-ui/src/app/edititem/edititem.component.ts
+++ adam-ui/src/app/edititem/edititem.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { ApirequestsService } from '../apirequests.service';
 
 @Component({
   selector: 'app-edititem',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdititemComponent implements OnInit {
 
-  constructor() { }
+  featured: any = {};
+  news: any = {};
+  compliancePages: Array<object> = [];
+
+  constructor(private _apirequestsService: ApirequestsService, private route: ActivatedRoute, private router: Router, public auth: AuthService) { }
 
   ngOnInit(): void {
+
+    this._apirequestsService.getFeatured()
+        .subscribe(featuredData => {
+        this.featured = featuredData.content;
+    });
+
+    this._apirequestsService.getNews()
+        .subscribe(newsdata => {
+        this.news = newsdata.content;
+        console.log(this.news);
+    });
+
+    this._apirequestsService.getCompliance()
+    .subscribe(data => {
+      this.compliancePages = data.content;
+    });
+
   }
 
 }
