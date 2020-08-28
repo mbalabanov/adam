5b498bd3743becc9dc12af9ba7892331c1b5d961
marinbalabanov <marincomics@gmail.com>
2020-08-28 09:34:42 +0300

    Corrected cookie warning, first preparations for editing archive items

--- src/app/components/itemdetails/itemdetails.component.ts
+++ src/app/components/itemdetails/itemdetails.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { ApirequestsService } from '../../services/apirequests.service';
+import { ArchiveItemClass, ArchiveItemDates, ArchiveItemImages, ArchiveItemVideos, ArchiveItemWebsiteURLs, ArchiveItemAssets } from '../../classes/archiveitemclass';
 
 @Component({
   selector: 'app-itemdetails',
@@ -10,34 +12,98 @@ import { ApirequestsService } from '../../services/apirequests.service';
 })
 export class ItemdetailsComponent implements OnInit {
 
-archiveCategory: string = '';
-archiveItem: any = {};
-pageUrl: string;
-relatedArtifacts: Array<object> = [];
-relatedPersons: Array<object> = [];
-relatedEvents: Array<object> = [];
 
-deleteThis(dataType, id) {
-    this.apirequestsService.deleteItem(dataType, id).subscribe((data)=>{
-        console.log('Request successful');
-   });
-}
+    // Variables for displaying archive item details
+    archiveCategory: string = '';
+    archiveItem: any = {};
+    pageUrl: string;
 
-setCarouselActiveID(id){
-    var elems = document.querySelectorAll('.active');
-    [].forEach.call(elems, function(el) {
-        el.classList.remove('active');
+    relatedArtifacts: Array<object> = [];
+    relatedPersons: Array<object> = [];
+    relatedEvents: Array<object> = [];
+
+    // Variables for editing archive item details
+    newArchiveItem: ArchiveItemClass = { 
+        id: '',
+        category: '',
+        name: '',
+        aliases: [],
+        shortdescription: '',
+        longdescription: '',
+        dates: [],
+        tags: [],
+        images: [],
+        videos: [],
+        websiteURLs: [],
+        assets: [],
+        artifacts: [],
+        persons: [],
+        events: []
+    };
+
+    newArchiveItemDates: ArchiveItemDates = {
+        label: '',
+        date: ''
+    };
+
+    newArchiveItemImages: ArchiveItemImages = {
+        id: '',
+        url: '',
+        name: '',
+        description: ''
+    };
+
+    newArchiveItemVideos: ArchiveItemVideos = {
+        id: '',
+        url: '',
+        name: '',
+        description: ''
+    };
+
+    newArchiveItemWebsiteURLs: ArchiveItemWebsiteURLs = {
+        id: '',
+        url: '',
+        name: '',
+    };
+
+    newArchiveItemAssets: ArchiveItemAssets = {
+        id: '',
+        url: '',
+        name: '',
+    };
+
+    newAliases: Array<string> = [];
+    newTags: Array<string> = [];
+    newRelatedArtifacts: Array<string> = [];
+    newRelatedPersons: Array<string> = [];
+    newRelatedEvents: Array<string> = [];
+
+    deleteThis(dataType, id) {
+        this.apirequestsService.deleteItem(dataType, id).subscribe((data)=>{
+            console.log('Request successful');
     });
-    var activeElem = document.getElementById(id);
-    activeElem.classList.add('active');
-};
 
-clipboardItemURL(itemurl) {
-    console.log(itemurl);
-    itemurl.select();  
-    document.execCommand('copy');  
-    itemurl.setSelectionRange(0, 0);  
-}
+    }
+
+    setCarouselActiveID(id){
+        var elems = document.querySelectorAll('.active');
+        [].forEach.call(elems, function(el) {
+            el.classList.remove('active');
+        });
+        var activeElem = document.getElementById(id);
+        activeElem.classList.add('active');
+    };
+
+    clipboardItemURL(itemurl) {
+        console.log(itemurl);
+        itemurl.select();  
+        document.execCommand('copy');  
+        itemurl.setSelectionRange(0, 0);  
+    }
+
+    replaceUnreadables(str, find, replace) {
+        return str.replace(new RegExp(find, 'g'), replace);
+    }
 
 constructor(private apirequestsService: ApirequestsService, private route: ActivatedRoute, public auth: AuthService ) { }
 
@@ -59,6 +125,11 @@ ngOnInit(): void {
                 this.apirequestsService.getEvent(itemSlug)
                 .subscribe(data => {
                     this.archiveItem = data;
+                    console.log(this.archiveItem);
+                    // Replace unreadable characters in longdescription.
+                    this.archiveItem.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '<EQUALS>', '=');
+                    this.archiveItem.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '<AMPERSAND>', '&');
+
                     for (let individualPerson of this.archiveItem.persons) {
                         this.apirequestsService.getPerson(individualPerson).subscribe(data => {
                             this.relatedPersons.push(data);
@@ -84,6 +155,11 @@ ngOnInit(): void {
                 this.apirequestsService.getPerson(itemSlug)
                 .subscribe(data => {
                     this.archiveItem = data;
+                    console.log(this.archiveItem);
+                    // Replace unreadable characters in longdescription.
+                    this.archiveItem.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '=', '<EQUALS>');
+                    this.archiveItem.value.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '&', '<AMPERSAND>');
+
                     for (let individualPerson of this.archiveItem.persons) {
                         this.apirequestsService.getPerson(individualPerson).subscribe(data => {
                             this.relatedPersons.push(data);
@@ -109,6 +185,11 @@ ngOnInit(): void {
                 this.apirequestsService.getArtifact(itemSlug)
                 .subscribe(data => {
                     this.archiveItem = data;
+                    console.log(this.archiveItem);
+                    // Replace unreadable characters in longdescription.
+                    this.archiveItem.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '=', '<EQUALS>');
+                    this.archiveItem.value.longdescription = this.replaceUnreadables(this.archiveItem.longdescription, '&', '<AMPERSAND>');
+
                     for (let individualPerson of this.archiveItem.persons) {
                         this.apirequestsService.getPerson(individualPerson).subscribe(data => {
                             this.relatedPersons.push(data);
