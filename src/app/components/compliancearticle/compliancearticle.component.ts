ceb1baa5d13bacb9ca3e8e5acb127bdab7a037af
marinbalabanov <marincomics@gmail.com>
2020-09-01 12:39:54 +0300

    Added validation to edit compliance pages

--- src/app/components/compliancearticle/compliancearticle.component.ts
+++ src/app/components/compliancearticle/compliancearticle.component.ts
@@ -27,7 +27,7 @@ export class CompliancearticleComponent implements OnInit {
         });
 
     });
-
+    
   }
 
 }
--- src/app/components/editcompliancepages/editcompliancepages.component.ts
+++ src/app/components/editcompliancepages/editcompliancepages.component.ts
@@ -23,46 +23,57 @@ export class EditcompliancepagesComponent implements OnInit {
       this.compliancePagesOriginal = data;
       this.compliancePagesItemsArray = this.compliancePagesOriginal.content;
 
+      console.log(this.compliancePagesItemsArray);
+
       this.compliancePagesEdited = new FormGroup({
         complianceabout: new FormGroup({
           id: new FormControl(this.compliancePagesItemsArray[0].id, Validators.required), 
           title: new FormControl(this.compliancePagesItemsArray[0].title, Validators.required),
-          firstimage: new FormControl(this.compliancePagesItemsArray[0].firstimage, Validators.required),
-          secondimage: new FormControl(this.compliancePagesItemsArray[0].secondimage, Validators.required),
+          firstimage: new FormControl(this.compliancePagesItemsArray[0].firstimage),
+          secondimage: new FormControl(this.compliancePagesItemsArray[0].secondimage),
           articletext: new FormControl(this.compliancePagesItemsArray[0].articletext, Validators.required)
         }),
         complianceprivacy: new FormGroup({
           id: new FormControl(this.compliancePagesItemsArray[1].id, Validators.required), 
           title: new FormControl(this.compliancePagesItemsArray[1].title, Validators.required),
-          firstimage: new FormControl(this.compliancePagesItemsArray[1].firstimage, Validators.required),
-          secondimage: new FormControl(this.compliancePagesItemsArray[1].secondimage, Validators.required),
+          firstimage: new FormControl(this.compliancePagesItemsArray[1].firstimage),
+          secondimage: new FormControl(this.compliancePagesItemsArray[1].secondimage),
           articletext: new FormControl(this.compliancePagesItemsArray[1].articletext, Validators.required)
         }),
         complianceterms: new FormGroup({
           id: new FormControl(this.compliancePagesItemsArray[2].id, Validators.required), 
           title: new FormControl(this.compliancePagesItemsArray[2].title, Validators.required),
-          firstimage: new FormControl(this.compliancePagesItemsArray[2].firstimage, Validators.required),
-          secondimage: new FormControl(this.compliancePagesItemsArray[2].secondimage, Validators.required),
+          firstimage: new FormControl(this.compliancePagesItemsArray[2].firstimage),
+          secondimage: new FormControl(this.compliancePagesItemsArray[2].secondimage),
           articletext: new FormControl(this.compliancePagesItemsArray[2].articletext, Validators.required)
         }),
         complianceimprint: new FormGroup({
           id: new FormControl(this.compliancePagesItemsArray[3].id, Validators.required), 
           title: new FormControl(this.compliancePagesItemsArray[3].title, Validators.required),
-          firstimage: new FormControl(this.compliancePagesItemsArray[3].firstimage, Validators.required),
-          secondimage: new FormControl(this.compliancePagesItemsArray[3].secondimage, Validators.required),
+          firstimage: new FormControl(this.compliancePagesItemsArray[3].firstimage),
+          secondimage: new FormControl(this.compliancePagesItemsArray[3].secondimage),
           articletext: new FormControl(this.compliancePagesItemsArray[3].articletext, Validators.required)
         }),
         compliancecookies: new FormGroup({
           id: new FormControl(this.compliancePagesItemsArray[4].id, Validators.required), 
           title: new FormControl(this.compliancePagesItemsArray[4].title, Validators.required),
-          firstimage: new FormControl(this.compliancePagesItemsArray[4].firstimage, Validators.required),
-          secondimage: new FormControl(this.compliancePagesItemsArray[4].secondimage, Validators.required),
+          firstimage: new FormControl(this.compliancePagesItemsArray[4].firstimage),
+          secondimage: new FormControl(this.compliancePagesItemsArray[4].secondimage),
           articletext: new FormControl(this.compliancePagesItemsArray[4].articletext, Validators.required)
         }),
       });
     });
 
   }
+
+    // Get the required fields for validation
+    get title() {
+      return this.compliancePagesEdited.get('title'); // The compliance page needs a title
+    }
+
+    get articletext() {
+      return this.compliancePagesEdited.get('articletext'); // The compliance page needs a text
+    }
 
   saveCompliancePages() {
 
